7853908ba965246ab409974d1b97ade16b9541ec
metao1 <mehrdadallahkarami@gmail.com>
2016-01-28 13:57:28 +0000

    secnd

--- app.js
+++ app.js
@@ -1,45 +1,21 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+    app = express(),
+    path = require('path'),
+    http = require('http').Server(app),
+    fs = require('fs'),
+    io = require('socket.io')(http);
 
-var routes = require('./routes/index');
-var app = express();
-
-app.use('/', routes);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+app.use(express.static(path.join(__dirname, './public')));
+io.sockets.on('connection', function (socket) {
+  socket.on('location', function (data) {
+    console.log(data);
+    data =  data.lat+","+data.lon +";;";   
+    fs.appendFile('data/location.log',data,function(err){
+       if(err) throw err;        
     });
   });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
 });
 
-
-module.exports = app;
+http.listen(8000, function () {
+    console.log('listening on: 8000');
+});
