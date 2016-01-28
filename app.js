c9aedfca951aba2f7da922f80c615c2c25018bca
metao1 <mehrdadallahkarami@gmail.com>
2016-01-28 14:43:35 +0000

    forth

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ var express = require('express'),
     fs = require('fs'),
     io = require('socket.io')(http);
 
+var items = Array("http://lorempixel.com/400/200/",
+                  "http://lorempixel.com/g/400/200","http://lorempixel.com/400/200/sports/",
+                  "http://lorempixel.com/400/200/sports/1/");
 app.use(express.static(path.join(__dirname, './public')));
 io.sockets.on('connection', function (socket) {
   socket.on('location', function (data) {
     console.log(data);
+    var item = items[Math.floor(Math.random()*items.length)];
+    socket.emit('data',item)  
     /*data =  data.lat+","+data.lon +";;";   
     fs.appendFile('data/location.log',data,function(err){
        if(err) throw err;        
@@ -18,5 +23,5 @@ io.sockets.on('connection', function (socket) {
 });
 
 http.listen(process.env.PORT || 5000, function () {
-    console.log('listening on: 8000');
+    console.log('listening on: 5000');
 });
